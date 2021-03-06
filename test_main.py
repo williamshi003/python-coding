434726bda5949d328b5824cfc0d6046b2b5976dc
william <williamshi003@163.com>
2021-03-06 10:55:39 +0800

    add div test

--- test_main.py
+++ test_main.py
@@ -4,9 +4,15 @@ def add(x, y):
 def sub(x, y):
     return x - y
 
+def div(a, b):
+    return a / b
+
 def test_add():
     assert add(1, 3) == 4
 
 
 def test_sub():
     assert sub(5, 3) == 2
+
+def test_div():
+    assert div(4, 2) == 2
